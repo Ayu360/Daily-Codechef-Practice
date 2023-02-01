f12ca25c219fcac8cf6aa6da8a603d27d72abf3b
Ayush Singh <81370467+Ayu360@users.noreply.github.com>
2023-02-01 22:24:33 +0530

    Create countOdd.cpp

---
+++ countOdd.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans=0;
+        for (int i=low; i<=high; i++){
+            if(i%2){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
