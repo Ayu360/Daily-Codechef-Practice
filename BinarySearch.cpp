79209946c000c7fa2fc389b72f93a4d4734ee1b7
Ayush Singh <81370467+Ayu360@users.noreply.github.com>
2023-02-13 23:08:11 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,15 @@
+// Problem Name: Binary Search
+// Problem Link: https://leetcode.com/problems/binary-search/?envType=study-plan&id=binary-search-i
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int idx = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+        if (idx > 0 && nums[idx - 1] == target) {
+            return idx - 1;
+        } else {
+            return -1;
+        }    
+    }
+};
